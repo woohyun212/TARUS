2eea8e8b6432857d24e19fd3c515282a2db5e41e
woohyun212 <woohyun212@gmail.com>
2021-04-20 21:22:02 +0900

    Project 관련 기능들 개발
    project 관련 메소드들 '일단은' 완성했다.
    
    이제 Task 를 작업할까
    
    내 몸이 너무 지친다!

--- todoist_api.py
+++ todoist_api.py
@@ -2,13 +2,40 @@ import requests, uuid, json
 
 
 class TodoistAPI:
-    def __init__(self, api_token):
+    def __init__(self, api_token):  # Project Method
         self.api_token = api_token
+        self.projects = {}
+        self.LoadProjects()
+        # you can get API token here
+        # https://todoist.com/prefs/integrations
 
-    # you can get API token here
-    # https://todoist.com/prefs/integrations
+    def LoadProjects(self):
+        for project in self.GetAllProjectData():
+            self.projects.update({project['name']: str(project['id'])})
 
-    def CreateNewProject(self, project_name):
+    def GetAllProjectData(self):  # Project Method
+        """
+
+        :return: list
+        """
+        return requests.get(
+            "https://api.todoist.com/rest/v1/projects",
+            headers={
+                "Authorization": "Bearer %s" % self.api_token}
+        ).json()
+
+    def GetSingleProjectData(self, project_id):  # Project Method
+        """
+
+        :type project_id: str
+        """
+        return requests.get(
+            "https://api.todoist.com/rest/v1/projects/" + project_id,
+            headers={
+                "Authorization": "Bearer %s" % self.api_token
+            }).json()
+
+    def CreateNewProject(self, project_name):  # Project Method
         """
 
         :type project_name: str
@@ -23,24 +50,35 @@ class TodoistAPI:
                 "X-Request-Id": str(uuid.uuid4()),
                 "Authorization": "Bearer %s" % self.api_token
             }).json()
+        self.LoadProjects()
 
-    def GetSingleProject(self):
-        return requests.get(
-            "https://api.todoist.com/rest/v1/projects",
+    def UpdateProjectName(self, project_id, project_name):  # Project Method
+        requests.post(
+            f"https://api.todoist.com/rest/v1/projects/{project_id}",
+            data=json.dumps({
+                "name": project_name
+            }),
             headers={
-                "Authorization": "Bearer %s" % self.api_token}
-        ).json()
+                "Content-Type": "application/json",
+                "X-Request-Id": str(uuid.uuid4()),
+                "Authorization": "Bearer %s" % self.api_token
+            })
+        self.LoadProjects()
 
-    def GetAProject(self, project_id):
-        """
+    def DeleteProject(self, project_id):  # Project Method
+        requests.delete(
+            f"https://api.todoist.com/rest/v1/projects/{project_id}",
+            headers={
+                "Authorization": "Bearer %s" % self.api_token
+            })
+        self.LoadProjects()
 
-        :type project_id: str
-        """
-        return requests.get(
-            "https://api.todoist.com/rest/v1/projects/" + project_id,
+    def GetAllCollaborators(self):  # Project Method
+        requests.get(
+            "https://api.todoist.com/rest/v1/projects/2203306141/collaborators",
             headers={
                 "Authorization": "Bearer %s" % self.api_token
             }).json()
 
-
-api = TodoistAPI(input("your API token : "))
+    def GetProjectId(self, project_name):  # Project Method
+        return self.projects[project_name]
